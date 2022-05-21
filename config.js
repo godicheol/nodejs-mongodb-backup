d225d722da3b3639805f1fd8e8334a9c4611b6fd
2cheol <godicheol@gmail.com>
2022-05-21 13:21:46 +0900

    1

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    host: "localhost",
+    port: "27017",
+    authenticationDatabase: undefined, // admin?
+    username: undefined,
+    password: undefined,
+    db: undefined, // database name
+    gzip: true,
+    os: "windows",
+}
---
+++ server.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var path = require('path');
+var { spawn } = require('child_process');
+var config = require('./config');
+var schedule = require('node-schedule');
+
+var exportDir = path.join(process.cwd(), "exports");
+
+var getDir = function() {
+    var currnetDate = new Date();
+    var year = currnetDate.getFullYear();
+    var month = currnetDate.getMonth();
+    var day = currnetDate.getDate();
+    var hours = currnetDate.getHours();
+    var minutes = currnetDate.getMinutes();
+    var seconds = currnetDate.getSeconds();
+
+    if (!fs.existsSync(exportDir)) {
+        fs.mkdirSync(exportDir);
+    }    
+
+    return path.join(exportDir,year+"-"+month+"-"+day+"T"+hours+"-"+minutes+"-"+seconds);
+}
+
+var backup = function() {
+    var options = [];
+    var options2 = {};
+    if (config.host) { options.push("--host="+config.host); }
+    if (config.port) { options.push("--port="+config.port); }
+    if (config.db) { options.push("--db="+config.db); }
+    if (config.authenticationDatabase) { options.push("--authenticationDatabase="+config.authenticationDatabase); }
+    if (config.username) { options.push("--username="+config.username); }
+    if (config.password) { options.push("--password="+config.password); }
+    if (config.gzip) { options.push("--gzip"); }
+    options.push("--out="+getDir());
+
+    if (config.os === "windows") { options2.shell = true; }
+
+    var backupProcess = spawn('mongodump', options, options2);
+
+    backupProcess.on('exit', function(code, signal) {
+        if(code) {
+            console.log('Backup process exited with code ', code);
+        } else if (signal) {
+            console.error('Backup process was killed with singal ', signal);
+        } else { 
+            console.log('Successfully backedup the database');
+        }
+    });
+}
+
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+var task = schedule.scheduleJob('0,10,20,30,40,50 * * * * *', function() {
+    try {
+        backup(); // test
+    } catch(err) {
+        console.log(err);
+    }
+});
